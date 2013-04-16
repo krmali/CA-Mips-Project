82d211c46a4a03acca49d2703c4b9aab827d66ff
Kareem <lkareem.alil@gmail.com>
2013-04-16 16:38:09 +0200

    the initial mips class

---
+++ Computer Architecture Project/src/pack/MIPS.java
@@ -0,0 +1,17 @@
+package pack;
+
+public class MIPS {
+
+	public String mux(String in1, String in2, String selector) {
+		return (selector == "0") ? in1 : in2;
+	}
+
+	public String shiftLeftTwo(String bin) {
+		return bin.substring(2) + "00";
+	}
+
+	public String signExtend(String bin){
+		return (bin.charAt(0) == '0') ? "0000000000000000" + bin: "1111111111111111"+bin; 
+	}
+	
+}
