0afc8de6e9a700c104e728b7184a3b2f338e2abb
Kareem <lkareem.alil@gmail.com>
2013-04-18 23:59:15 +0200

    adds the alu control unit

---
+++ Computer Architecture Project/src/pack/ALUControl.java
@@ -0,0 +1,33 @@
+package pack;
+
+import java.util.HashMap;
+
+public class ALUControl {
+	
+	String ALUOp;
+	String AluControlOutput;
+	HashMap <String, String> possibleInputs;
+	
+	public ALUControl(){
+		possibleInputs = new HashMap<String,String>();
+	}
+	
+	public void ALUIControlInput(String ALUOp) {
+		this.ALUOp = ALUOp;
+	}
+	
+	public void process (){
+		possibleInputs.put("0000", "and");
+		possibleInputs.put("0001","or");
+		possibleInputs.put("0010", "add");
+		possibleInputs.put("0110","subtract");
+		possibleInputs.put("0111", "slt");
+		possibleInputs.put("1100","nor");	
+		AluControlOutput = possibleInputs.get(ALUOp);
+	}
+	
+	public String ALUControlOutput(){
+		return AluControlOutput;
+	}
+
+}
