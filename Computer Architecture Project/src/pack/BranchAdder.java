5a2fd6a3b21d60d68f0a0069289a3926c68f68e4
Kareem <lkareem.alil@gmail.com>
2013-05-07 15:04:09 +0200

    [kareem] finishes the branch and jump

--- Computer Architecture Project/src/pack/BranchAdder.java
+++ Computer Architecture Project/src/pack/BranchAdder.java
@@ -10,11 +10,12 @@ public class BranchAdder {
 	}
 
 	public void branch(){
+		processJump();
 		String temp = MIPSWires.toShift2.substring(2);
 		temp += "00";
 		String result = addBinaryString(MIPSWires.pcOut, temp);
-		
-		  
+		boolean branchAnd = (MIPSWires.zero == "1" && MIPSWires.Branch == "1")? true : false;
+		MIPSWires.pcOut = branchAnd? result : MIPSWires.pcOut ;
 	}
 
 	public String addBinaryString(String s1, String s2) {
@@ -22,13 +23,10 @@ public class BranchAdder {
 				.add(new BigInteger(s2, 2))).toString()));
 	}
 
-	public void addBinaryString() {
-
-	}
 
 	public void processJump() {
 		jumpInput = MIPSWires.pcOut.substring(0, 4) + jumpInput + "00";
-		MIPSWires.pcOut = MIPSWires.Jump == "0" ? MIPSWires.pcIn : jumpInput;
+		MIPSWires.pcIn = MIPSWires.Jump == "0" ? MIPSWires.pcOut : jumpInput;
 	}
 
 	public static void main(String[] args) {
